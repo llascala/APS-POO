411d483d0ce358f0261838e92ec8597d4e461562
llascala <39611629+llascala@users.noreply.github.com>
2018-05-26 04:22:35 -0300

    Update ContaCorrente.java

--- src/banco/ContaCorrente.java
+++ src/banco/ContaCorrente.java
@@ -1,50 +1,54 @@
 package banco;
 
 public class ContaCorrente extends Conta {
-	
-	private double Limite;
-	
-	//Construtores
-	
-	public ContaCorrente(int numeroConta, String nomeCliente, int CPF) {
-		super(numeroConta, nomeCliente, CPF);
-	}
-	
-	public ContaCorrente(int numeroConta, String nomeCliente, int CPF,double Limite){
-		super(numeroConta, nomeCliente, CPF);
-		this.Limite = Limite;
-	}
-	
-	//Get e Set
-	
-	public double getLimite() {
-		return Limite;
-	}
 
-	public void setLimite(double limite) {
-		Limite = limite;
-	}
-	
-	//Metodos
-	
-	public boolean usandoLimite() {
-		if (Saldo < 0) {
-			return true;
-		} else {
-			return false;
-		}
-	}
-	
-	@Override
-	public boolean sacar(int numeroConta ,double valorSacado) {
-		return super.sacar(numeroConta, valorSacado);
-		//Considerar limite nesse ponto
-	}
-	
-	@Override
-	public String Imprimir() {
-		return super.Imprimir() + "\nLimite: " + Limite;
-	}
-	
-	
+    private double Limite;
+
+    //Construtores
+
+    public ContaCorrente() {
+    }
+    
+    
+    public ContaCorrente(int numeroConta, String nomeCliente, int CPF) {
+        super(numeroConta, nomeCliente, CPF);
+    }
+
+    public ContaCorrente(int numeroConta, String nomeCliente, int CPF, double Limite) {
+        super(numeroConta, nomeCliente, CPF);
+        this.Limite = Limite;
+    }
+
+    //Get e Set
+    public double getLimite() {
+        return Limite;
+    }
+
+    public void setLimite(double limite) {
+        Limite = limite;
+    }
+
+    //Metodos 
+    public boolean usandoLimite() {
+        if (Saldo < 0) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    @Override
+    public boolean sacar(int numeroConta, double valorSacado) {
+        if (valorSacado < Limite) {
+            super.sacar(numeroConta, valorSacado);
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    @Override
+    public String Imprimir() {
+        return super.Imprimir() + "\nLimite: " + Limite;
+    }
 }
